d4698c3872c924b4438a06d937955e7ef193c27d
Rusal Shrestha <rusalo101@gmail.com>
2021-04-14 20:14:14 -0400

    added state to the ExpenseItem component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [ title, setTitle ] = useState(props.title)
+
+  const changeTitleHandler = () => {
+    setTitle("New Title");
+    console.log(title);
+  };
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={changeTitleHandler}>Change Title</button>
       </div>
     </Card>
   );
